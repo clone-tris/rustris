11d43bfe1739f2e4718426c4c31001052b771296
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-09-15 13:50:31 +0200

    start using a velocity approach

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,172 @@
-use ggez::event;
 use ggez::event::KeyCode;
 use ggez::graphics;
-use ggez::graphics::{Mesh, Rect};
+use ggez::graphics::{DrawParam, Font, Mesh, Rect};
 use ggez::input::keyboard;
 use ggez::input::keyboard::KeyMods;
 use ggez::nalgebra as na;
 use ggez::{self, conf};
+use ggez::{event, timer};
 use ggez::{Context, GameResult};
+use std::env;
+use std::path;
+use std::time::Duration;
 
-const PLAYER_SIZE: f32 = 100.0;
+const DESIRED_FPS: u32 = 60;
+const WIN_WIDTH: f32 = 800.0;
+const WIN_HEIGHT: f32 = 460.0;
 
-enum Translate {
+enum Force {
     Up,
     Down,
     Left,
     Right,
+    Resistence,
 }
 
+#[derive(Debug)]
 struct Player {
     x: f32,
     y: f32,
     size: f32,
+    acceleration: f32,
+    max_velocity: f32,
+    x_velocity: f32,
+    y_velocity: f32,
+    no_x_force: bool,
+    no_y_force: bool,
 }
 
 impl Player {
     fn new(size: f32) -> Player {
-        Player {
+        let player = Player {
             size,
             x: 0.0,
             y: 0.0,
-        }
+            acceleration: 2.0,
+            max_velocity: 10.0,
+            x_velocity: 0.0,
+            y_velocity: 0.0,
+            no_x_force: true,
+            no_y_force: true,
+        };
+
+        println!("{:?}", player);
+
+        return player;
     }
-    fn center_player(&mut self, width: f32, height: f32) {
-        self.x = width / 2.0 - self.size / 2.0;
-        self.y = height / 2.0 - self.size / 2.0;
+    fn center_player(&mut self) {
+        self.x = WIN_WIDTH / 2.0 - self.size / 2.0;
+        self.y = WIN_HEIGHT / 2.0 - self.size / 2.0;
     }
 }
 
 struct MainState {
     player: Player,
-    width: f32,
-    height: f32,
+    text: graphics::Text,
 }
 
 impl MainState {
-    fn new(width: f32, height: f32) -> GameResult<MainState> {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
         let mut player = Player::new(100.0);
 
-        player.center_player(width, height);
+        player.center_player();
 
-        let s = MainState {
-            width,
-            height,
-            player,
-        };
+        let font = Font::new(ctx, "/SourceCodePro-Regular.ttf")?;
+        let text = graphics::Text::new(("Hello world!", font, 48.0));
+
+        let s = MainState { player, text };
         Ok(s)
     }
 
-    fn translate(&mut self, direction: Translate, distance: f32) {
+    fn apply_force(&mut self, direction: Force, dt: Duration) {
+        let player = &mut self.player;
+        let dt_f32 = dt.as_secs_f32();
+
+        player.y += player.y_velocity;
+        player.x += player.x_velocity;
+
+        if player.x < -player.size {
+            player.x = WIN_WIDTH;
+        } else if player.x > WIN_WIDTH {
+            player.x = -player.size;
+        }
+        if player.y < -player.size {
+            player.y = WIN_HEIGHT;
+        } else if player.y > WIN_HEIGHT {
+            player.y = -player.size;
+        }
+
         match direction {
-            Translate::Up => {
-                self.player.y += -1.0 * distance;
+            Force::Up => {
+                player.y_velocity -= player.acceleration * dt_f32;
+                player.no_y_force = false;
             }
-            Translate::Down => {
-                self.player.y += distance;
+            Force::Down => {
+                player.y_velocity += player.acceleration * dt_f32;
+                player.no_y_force = false;
             }
-            Translate::Left => {
-                self.player.x += -1.0 * distance;
+            Force::Left => {
+                player.x_velocity -= player.acceleration * dt_f32;
+                player.no_x_force = false;
             }
-            Translate::Right => {
-                self.player.x += distance;
+            Force::Right => {
+                player.x_velocity += player.acceleration * dt_f32;
+                player.no_x_force = false;
+            }
+            Force::Resistence => {
+                if player.x_velocity != 0.0 && player.no_x_force {
+                    let u_x = player.x_velocity / player.x_velocity.abs();
+                    player.x_velocity -= u_x * dt_f32 * player.acceleration;
+                    if u_x * player.x_velocity < 0.0 {
+                        player.x_velocity = 0.0;
+                    }
+                }
+                if player.y_velocity != 0.0 && player.no_y_force {
+                    let u_y = player.y_velocity / player.y_velocity.abs();
+                    player.y_velocity -= u_y * dt_f32 * player.acceleration;
+                    if u_y * player.y_velocity < 0.0 {
+                        player.y_velocity = 0.0;
+                    }
+                }
+                player.no_x_force = true;
+                player.no_y_force = true;
             }
         }
-        if self.player.x < -self.player.size {
-            self.player.x = self.width;
-        } else if self.player.x > self.width {
-            self.player.x = -self.player.size;
+
+        if player.x_velocity > player.max_velocity {
+            player.x_velocity = player.max_velocity
         }
-        if self.player.y < -self.player.size {
-            self.player.y = self.height;
-        } else if self.player.y > self.height {
-            self.player.y = -self.player.size;
+        if player.x_velocity < -player.max_velocity {
+            player.x_velocity = -player.max_velocity
+        }
+        if player.y_velocity > player.max_velocity {
+            player.y_velocity = player.max_velocity
+        }
+        if player.y_velocity < -player.max_velocity {
+            player.y_velocity = -player.max_velocity
         }
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        if keyboard::is_key_pressed(ctx, KeyCode::W) {
-            self.translate(Translate::Up, 5.0);
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::A) {
-            self.translate(Translate::Left, 5.0);
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::S) {
-            self.translate(Translate::Down, 5.0);
-        }
-        if keyboard::is_key_pressed(ctx, KeyCode::D) {
-            self.translate(Translate::Right, 5.0);
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            let dt = timer::delta(ctx);
+
+            self.apply_force(Force::Resistence, dt);
+
+            if keyboard::is_key_pressed(ctx, KeyCode::W) {
+                self.apply_force(Force::Up, dt);
+            }
+            if keyboard::is_key_pressed(ctx, KeyCode::A) {
+                self.apply_force(Force::Left, dt);
+            }
+            if keyboard::is_key_pressed(ctx, KeyCode::S) {
+                self.apply_force(Force::Down, dt);
+            }
+            if keyboard::is_key_pressed(ctx, KeyCode::D) {
+                self.apply_force(Force::Right, dt);
+            }
         }
         Ok(())
     }
@@ -120,40 +189,23 @@ impl event::EventHandler for MainState {
             (na::Point2::new(self.player.x, self.player.y),),
         )?;
 
+        let text_position = mint::Point2 { x: 5.0, y: 5.0 };
+        graphics::draw(ctx, &self.text, (text_position,))?;
+
         graphics::present(ctx)?;
         Ok(())
-    }
-
-    fn key_down_event(
-        &mut self,
-        ctx: &mut Context,
-        key: KeyCode,
-        _: KeyMods,
-        _: bool,
-    ) {
-        match key {
-            KeyCode::Escape => {
-                ggez::event::quit(ctx);
-            }
-            KeyCode::Right => {
-                self.translate(Translate::Right, self.player.size / 2.0)
-            }
-            KeyCode::Left => {
-                self.translate(Translate::Left, self.player.size / 2.0)
-            }
-            KeyCode::Up => {
-                self.translate(Translate::Up, self.player.size / 2.0)
-            }
-            KeyCode::Down => {
-                self.translate(Translate::Down, self.player.size / 2.0)
-            }
-            _ => (),
-        }
     }
 }
 
 pub fn main() -> GameResult {
-    let state = &mut MainState::new(800.0, 460.0)?;
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR")
+    {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
+    };
 
     let cb = ggez::ContextBuilder::new("rustris", "Abderrahmane Tahri Jouti")
         .window_setup(
@@ -161,9 +213,13 @@ pub fn main() -> GameResult {
                 .title("Clonetris, made easy, in Rust!"),
         )
         .window_mode(
-            conf::WindowMode::default().dimensions(state.width, state.height),
-        );
+            conf::WindowMode::default().dimensions(WIN_WIDTH, WIN_HEIGHT),
+        )
+        .add_resource_path(resource_dir);
 
     let (ctx, event_loop) = &mut cb.build()?;
+
+    let state = &mut MainState::new(ctx)?;
+
     event::run(ctx, event_loop, state)
 }
