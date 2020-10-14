e3d3a5bdc76f99ace727e1db4e3d486b37eb8ee8
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2020-10-14 20:36:13 +0200

    double buffering 🎉 (❁´◡`❁)(⌐■_■)( •_•)>⌐■-■

--- src/framework/mod.rs
+++ src/framework/mod.rs
@@ -1,17 +1,28 @@
+use ggez;
+use nalgebra;
+
 use ggez::event::EventHandler;
+use ggez::graphics::{Canvas, Color, DrawParam};
 use ggez::input::keyboard;
 use ggez::input::keyboard::KeyCode;
 use ggez::{graphics, timer, Context, GameResult};
+use ggez::{nalgebra as na, GameError};
 
 trait Scene {
     fn update(&self, ctx: &mut Context);
+    fn paint(&self, ctx: &mut Context);
+    fn get_canvas(&self) -> &Canvas;
 }
 
-struct MenuScene {}
+struct MenuScene {
+    canvas: graphics::Canvas,
+}
 
 impl MenuScene {
-    fn new() -> MenuScene {
-        MenuScene {}
+    fn new(ctx: &mut Context) -> GameResult<MenuScene> {
+        Ok(MenuScene {
+            canvas: graphics::Canvas::with_window_size(ctx)?,
+        })
     }
 }
 
@@ -22,16 +33,23 @@ impl Scene for MenuScene {
             println!("{}", time);
         }
     }
+    fn paint(&self, ctx: &mut Context) {
+        graphics::clear(ctx, Color::from((80, 150, 200, 128)));
+    }
+
+    fn get_canvas(&self) -> &Canvas {
+        &self.canvas
+    }
 }
 
 pub struct Game {
-    scene: Box<dyn Scene>,
+    pub scene: Box<dyn Scene>,
 }
 
 impl Game {
     pub fn new(ctx: &mut Context) -> GameResult<Game> {
-        let scene = Box::new(MenuScene::new());
-        let mut game = Game { scene };
+        let scene = Box::new(MenuScene::new(ctx)?);
+        let game = Game { scene };
         Ok(game)
     }
 }
@@ -41,9 +59,15 @@ impl EventHandler for Game {
         self.scene.update(ctx);
         Ok(())
     }
-
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, graphics::BLACK);
+        let canvas = self.scene.get_canvas();
+
+        graphics::set_canvas(ctx, Some(canvas));
+        self.scene.paint(ctx);
+
+        graphics::set_canvas(ctx, None);
+        graphics::draw(ctx, canvas, DrawParam::new());
+
         graphics::present(ctx)?;
         Ok(())
     }
