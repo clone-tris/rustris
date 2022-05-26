097b318cd48e603dd6d47251a6d03b73897264b1
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2022-05-26 17:11:17 +0200

    paint the ugly colors

--- src/screens/game/components/square.rs
+++ src/screens/game/components/square.rs
@@ -1,4 +1,6 @@
-use crate::main_config::SQUARE_WIDTH;
+use crate::colors::ShapeColors::{BorderBottom, BorderSide, BorderTop};
+use crate::main_config::{SQUARE_BORDER_WIDTH, SQUARE_WIDTH};
+use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
@@ -16,86 +18,66 @@ impl Square {
     }
 
     pub fn draw(&self, canvas: &mut WindowCanvas, reference_row: i32, reference_column: i32) {
-        let y = (self.row + reference_row) * SQUARE_WIDTH;
-        let x = (self.column + reference_column) * SQUARE_WIDTH;
+        let y = ((self.row + reference_row) * SQUARE_WIDTH) as i16;
+        let x = ((self.column + reference_column) * SQUARE_WIDTH) as i16;
+        const BORDER_WIDTH: i16 = SQUARE_BORDER_WIDTH as i16;
+        const WIDTH: i16 = SQUARE_WIDTH as i16;
 
         // // Background
         canvas.set_draw_color(self.color);
         canvas
-            .fill_rect(Rect::new(x, y, SQUARE_WIDTH as u32, SQUARE_WIDTH as u32))
+            .fill_rect(Rect::new(
+                x as i32,
+                y as i32,
+                SQUARE_WIDTH as u32,
+                SQUARE_WIDTH as u32,
+            ))
             .unwrap();
 
         // Left Border
-        // mb.polygon(
-        //     DrawMode::fill(),
-        //     &[
-        //         glam::Vec2::new(x, y),
-        //         glam::Vec2::new(x + square_border_width, y + square_border_width),
-        //         glam::Vec2::new(
-        //             x + square_border_width,
-        //             y + square_width - square_border_width,
-        //         ),
-        //         glam::Vec2::new(x, y + square_width),
-        //     ],
-        //     ShapeColors::BorderSide.value(),
-        // )
-        // .unwrap();
+        canvas
+            .filled_polygon(
+                &[x, x + BORDER_WIDTH, x + BORDER_WIDTH, x],
+                &[y, y + BORDER_WIDTH, y + WIDTH - BORDER_WIDTH, y + WIDTH],
+                BorderSide.value(),
+            )
+            .unwrap();
 
         // Right Border
-        // mb.polygon(
-        //     DrawMode::fill(),
-        //     &[
-        //         glam::Vec2::new(x + square_width, y),
-        //         glam::Vec2::new(
-        //             x + square_width - square_border_width,
-        //             y + square_border_width,
-        //         ),
-        //         glam::Vec2::new(
-        //             x + square_width - square_border_width,
-        //             y + square_width - square_border_width,
-        //         ),
-        //         glam::Vec2::new(x + square_width, y + square_width),
-        //     ],
-        //     ShapeColors::BorderSide.value(),
-        // )
-        // .unwrap();
+        canvas
+            .filled_polygon(
+                &[
+                    x + WIDTH,
+                    x + WIDTH - BORDER_WIDTH,
+                    x + WIDTH - BORDER_WIDTH,
+                    x + WIDTH,
+                ],
+                &[y, y + BORDER_WIDTH, y + WIDTH - BORDER_WIDTH, y + WIDTH],
+                BorderSide.value(),
+            )
+            .unwrap();
 
         // Top Border
-        // mb.polygon(
-        //     DrawMode::fill(),
-        //     &[
-        //         glam::Vec2::new(x, y),
-        //         glam::Vec2::new(x + square_border_width, y + square_border_width),
-        //         glam::Vec2::new(
-        //             x + square_width - square_border_width,
-        //             y + square_border_width,
-        //         ),
-        //         glam::Vec2::new(x + square_width, y),
-        //     ],
-        //     ShapeColors::BorderTop.value(),
-        // )
-        // .unwrap();
+        canvas
+            .filled_polygon(
+                &[x, x + BORDER_WIDTH, x + WIDTH - BORDER_WIDTH, x + WIDTH],
+                &[y, y + BORDER_WIDTH, y + BORDER_WIDTH, y],
+                BorderTop.value(),
+            )
+            .unwrap();
 
         // Bottom Border
-        // mb.polygon(
-        //     DrawMode::fill(),
-        //     &[
-        //         glam::Vec2::new(x, y + square_width),
-        //         glam::Vec2::new(
-        //             x + square_border_width,
-        //             y + square_width - square_border_width,
-        //         ),
-        //         glam::Vec2::new(
-        //             x + square_width - square_border_width,
-        //             y + square_width - square_border_width,
-        //         ),
-        //         glam::Vec2::new(x + square_width, y + square_width),
-        //     ],
-        //     ShapeColors::BorderBottom.value(),
-        // )
-        // .unwrap();
-
-        // let mesh = mb.build(ctx).unwrap();
-        // graphics::draw(ctx, &mesh, DrawParam::default()).unwrap();
+        canvas
+            .filled_polygon(
+                &[x, x + BORDER_WIDTH, x + WIDTH - BORDER_WIDTH, x + WIDTH],
+                &[
+                    y + WIDTH,
+                    y + WIDTH - BORDER_WIDTH,
+                    y + WIDTH - BORDER_WIDTH,
+                    y + WIDTH,
+                ],
+                BorderBottom.value(),
+            )
+            .unwrap();
     }
 }
